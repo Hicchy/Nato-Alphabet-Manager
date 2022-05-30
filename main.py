60363ed60b412207e7bdb1317c4f774ce9c485ae
Hicch_Master <work.halilibrahimcifci@gmail.com>
2022-05-30 15:30:18 +0300

    Added a while loop to keep the code going until success

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import pandas
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
-question = input("Enter a word: ").upper()
 
 
-try:
-    nato_alphabet = {row.letter: row.code for (index, row) in data.iterrows()}
-    result = [nato_alphabet[x] for x in question]
-except KeyError:
-    print(f"{question} is not a valid input")
-else:
-    print(result)
+continues = True
+
+
+while continues:
+    try:
+        question = input("Enter a word: ").upper()
+        nato_alphabet = {row.letter: row.code for (index, row) in data.iterrows()}
+        result = [nato_alphabet[x] for x in question]
+    except KeyError:
+        print(f"{question} is not a valid input")
+    else:
+        print(result)
+        continues = False
 
 
 
