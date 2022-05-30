be48c5b4f14ccaa595a91a5b85d3d4a87b41635d
Hicch_Master <work.halilibrahimcifci@gmail.com>
2022-05-30 16:05:45 +0300

    Wrapped everything around in a function

--- main.py
+++ main.py
@@ -3,21 +3,21 @@ data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 
 
-continues = True
-
-
-while continues:
-    try:
-        question = input("Enter a word: ").upper()
-        nato_alphabet = {row.letter: row.code for (index, row) in data.iterrows()}
-        result = [nato_alphabet[x] for x in question]
-    except KeyError:
-        print(f"{question} is not a valid input")
-    else:
-        print(result)
-        continues = False
 
 
 
+def phonetise():
+    continues = True
+    while continues:
+        try:
+            question = input("Enter a word: ").upper()
+            nato_alphabet = {row.letter: row.code for (index, row) in data.iterrows()}
+            result = [nato_alphabet[x] for x in question]
+        except KeyError:
+            print(f"{question} is not a valid input")
+        else:
+            print(result)
+            continues = False
 
 
+phonetise()
