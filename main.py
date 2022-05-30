247e96347360d8efc9187f721666eb69912bb1e3
Hicch_Master <work.halilibrahimcifci@gmail.com>
2022-05-30 15:00:19 +0300

    Added exception handling

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import pandas
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
-nato_alphabet = {row.letter: row.code for (index, row) in data.iterrows()}
-print(nato_alphabet)
-
 question = input("Enter a word: ").upper()
 
-result = [nato_alphabet[x] for x in question]
-print(result)
+
+try:
+    nato_alphabet = {row.letter: row.code for (index, row) in data.iterrows()}
+    result = [nato_alphabet[x] for x in question]
+    print(result)
+except KeyError as error_1:
+    print(f"{error_1} is not a valid letter.")
+
+
+
+
 
